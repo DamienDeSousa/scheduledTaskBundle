250a4f030b11652687babdec009f812744e044b7
Damien DE SOUSA <damien.desousa@smile.fr>
2020-02-14 13:56:55 +0100

    add repository injection

--- Repository/ScheduledCommandRepository.php
+++ Repository/ScheduledCommandRepository.php
@@ -2,13 +2,19 @@
 /**
  * Repository used to query scheduled command entities.
  *
- * @author    Damien DE SOUSA <de.sousa.pro@gmail.com>
+ * @author    Damien DE SOUSA <de.sousa.damien.pro@gmail.com>
  * @copyright 2020
  */
 namespace Dades\ScheduledTaskBundle\Repository;
 
+use Dades\ScheduledTaskBundle\Entity\ScheduledCommandEntity;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
 
 class ScheduledCommandRepository extends ServiceEntityRepository
 {
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, ScheduledCommandEntity::class);
+    }
 }
