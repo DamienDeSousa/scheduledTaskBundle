28ed54b48ef6b9143bf72ab5f2e7ac60b49ab28c
Damien DE SOUSA <damien.desousa@smile.fr>
2020-02-18 11:28:23 +0100

    testing unit tests

--- Tests/Service/ScheduledSymfonyCommandServiceTest.php
+++ Tests/Service/ScheduledSymfonyCommandServiceTest.php
@@ -2,6 +2,7 @@
 
 namespace Dades\ScheduledTaskBundle\Tests\Service;
 
+use Dades\ScheduledTaskBundle\Entity\ScheduledCommandEntity;
 use Dades\ScheduledTaskBundle\Repository\ScheduledCommandRepository;
 use Dades\ScheduledTaskBundle\Service\ScheduledSymfonyCommandService;
 use Doctrine\Common\Persistence\ObjectRepository;
@@ -14,31 +15,85 @@ use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
  */
 class ScheduledSymfonyCommandServiceTest extends KernelTestCase
 {
-    public function testCreate()
+    protected $projectDir;
+    protected $scheduledCommandType;
+    protected $scheduledCommandRepository;
+    protected $entityManager;
+
+    public function setUp()
     {
         $kernel = self::bootKernel();
-        $projectDir = $kernel->getProjectDir();
-        $scheduledCommandType = $kernel->getContainer()
+        $this->projectDir = $kernel->getProjectDir();
+        $this->scheduledCommandType = $kernel->getContainer()
             ->getParameter('dades_scheduled_task_bundle.scheduled_command_type.symfony');
-        $scheduledCommandRepository = $this->createMock(ScheduledCommandRepository::class);
-        $entityManager = $this->createMock(EntityManager::class);
-        $entityManager->expects($this->any())
+        $this->scheduledCommandRepository = $this->createMock(ScheduledCommandRepository::class);
+        $this->entityManager = $this->createMock(EntityManager::class);
+        $this->entityManager->expects($this->any())
             ->method('getRepository')
-            ->willReturn($scheduledCommandRepository);
+            ->willReturn($this->scheduledCommandRepository);
+    }
+
+    public function testConstruct()
+    {
+        try {
+            $scheduledSymfonyCommandService = new ScheduledSymfonyCommandService(
+                $this->entityManager,
+                $this->scheduledCommandRepository,
+                $this->projectDir,
+                $this->scheduledCommandType
+            );
+            $this->assertTrue(true);
+        } catch (\Exception $e) {
+            $this->assertFalse(true, $e->getMessage());
+        }
+    }
+
+    public function testCreate()
+    {
         $scheduledSymfonyCommandService = new ScheduledSymfonyCommandService(
-            $entityManager,
-            $scheduledCommandRepository,
-            $projectDir,
-            $scheduledCommandType
+            $this->entityManager,
+            $this->scheduledCommandRepository,
+            $this->projectDir,
+            $this->scheduledCommandType
         );
         $scheduledSymfonyCommandEntity = $scheduledSymfonyCommandService->create();
 
-        $this->assertEquals($scheduledCommandType, $scheduledSymfonyCommandEntity->getScheduledCommandEntityType());
+        $this->assertEquals($this->scheduledCommandType, $scheduledSymfonyCommandEntity->getScheduledCommandEntityType());
     }
+
+    public function testSave()
+    {
+        $scheduledSymfonyCommandService = new ScheduledSymfonyCommandService(
+            $this->entityManager,
+            $this->scheduledCommandRepository,
+            $this->projectDir,
+            $this->scheduledCommandType
+        );
+        $scheduledSymfonyCommandEntity = $scheduledSymfonyCommandService->create();
+        $scheduledSymfonyCommandEntity->setCommandName('about')
+            ->setCronExpression('*/ * * * *');
+        $scheduledSymfonyCommandService->save($scheduledSymfonyCommandEntity);
+
+        /** @var null|ScheduledCommandEntity $result */
+        $result = $this->scheduledCommandRepository->findOneBy(
+            ['commandName' => $scheduledSymfonyCommandEntity->getCommandName()]
+        );
+
+        $this->assertEquals($scheduledSymfonyCommandEntity->getCommandName(), $result->getCommandName());
+    }
+
+//    public function testUpdate()
+//    {
+//
+//    }
+//
+//    public function delete()
+//    {
+//
+//    }
 
     protected function tearDown()
     {
         parent::tearDown();
     }
-    // test save, update, delete
 }
